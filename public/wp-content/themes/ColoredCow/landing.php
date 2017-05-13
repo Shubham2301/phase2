6cc43403a785fb262c1abda95e26ee397eb29f92
shubham joshi <shubhamjoshi@shubhams-MacBook-Pro.local>
2017-05-13 18:24:02 +0530

    guest post displayed

--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -8,7 +8,15 @@
 ?>
 
 
-
+<?php $args = array( 'post_type' => 'guest', 'posts_per_page' => 10 );
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+  the_title();
+  echo '<div class="entry-content">';
+  the_content();
+  echo '</div>';
+endwhile;
+?>
 
 <?php 
     get_footer(); 
