06eb3b244956c3eaba65424366f080248e15a4e7
Shubham Joshi <shubhamjoshi2301@gmail.com>
2017-05-26 10:56:56 +0530

    #6 code correction

--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -6,34 +6,34 @@
 
 <?php get_header(); ?>
 <?php
-	$args = array( 'post_type' => 'Soiree', 'posts_per_page' => 1 );
+	$args = array( 'post_type' => 'soiree', 'posts_per_page' => 1 );
 	$loop = new WP_Query( $args );
 	while ( $loop->have_posts() ) : $loop->the_post();
-		echo '<div class="soiree_name">';
-			echo '<h1>';
-				the_title(); 
-			echo '</h1>';
-		echo '</div>';
-		echo '<div class="entry-content">';
-			echo '<h3>';
-				the_content();
-			echo '</h3>';	
-		echo '</div>';
-		echo '<div class="soiree_date">';
-			echo '<span class="event_date">Soiree Date:</span>';
-			the_field('event_date');
-		echo "</div>";		
-		echo '<div class="soiree_date">';
-			echo '<span class="last_date">Last Registration Date:</span>';
-			the_field('last_date');
-		echo "</div>";	
-		echo '<div class="link">';
-			echo'<a href=" http://public.dev/rsvp/" target="_blank">Click Here to RSVP for this Event</a>';
-		echo '</div>';
-		echo '<div class="link">';
-			echo'<a href=" http://public.dev/register/" target="_blank">Click Here to Register for this Event</a>';
-		echo '</div>';
-	endwhile;	
-?>
+?>	
+		<div class="soiree_name">
+			<h1>
+				<?php echo get_the_title();  ?>
+			</h1>
+		</div>
+		<div class="entry-content">
+			<h3>
+				<?php get_the_content(); ?>
+			</h3>	
+		</div>
+		<div class="soiree_date">
+			<span class="event_date">Soiree Date:</span>
+			<?php get_field('event_date'); ?>
+		</div>		
+		<div class="soiree_date">
+			<span class="last_date">Last Registration Date:</span>
+			<?php get_field('last_date'); ?>
+		</div>	
+		<div class="link">
+			<a href=" http://public.dev/rsvp/" target="_blank">Click Here to RSVP for this Event</a>
+		</div>
+		<div class="link">
+			<a href=" http://public.dev/register/" target="_blank">Click Here to Register for this Event</a>
+		</div>
+	<?php endwhile; ?>	
 
 <?php get_footer(); ?>
--- public/wp-content/themes/ColoredCow/src/js/main.js
+++ public/wp-content/themes/ColoredCow/src/js/main.js
@@ -8,10 +8,8 @@ jQuery(document).ready(function() {
     });
 
     jQuery('#submit_field').on('click', function() {
-        console.log("submit field clicked");
         RSVP();
     });
-
 });
 
 
@@ -21,38 +19,28 @@ function addSubscriber() {
         add_subscriber_form[0].reportValidity();
         return;
     }
-    console.log('clicked');
-    console.log(jQuery('#add_subscriber_form').serialize());
     jQuery.ajax({
         type: "POST",
         url: PARAMS.ajaxurl,
         data: add_subscriber_form.serialize(),
         success: function(response) {
-            console.log(response);
             if (response == 1) {
                 window.alert("Congratulations!!! you have successfully registered for this event");
             } else if (response == 0) {
                 window.alert("ERROR! these credentials are already registered");
             }
-
         }
     });
 }
 
 function RSVP() {
-    console.log("click verify");
     var verification_form = jQuery('#verification_form');
     var email1 = jQuery("#guest_email").val();
-    console.log(email1);
     var dataString = verification_form.serialize();
     if (!verification_form[0].checkValidity()) {
         verification_form[0].reportValidity();
         return;
     }
-    // var dataString = {
-    //     'action': 'verify_field',
-    //     'value': email1
-    // }
     console.log(PARAMS.ajaxurl);
     jQuery.ajax({
         type: "POST",
@@ -66,5 +54,4 @@ function RSVP() {
             }
         }
     });
-
 }
