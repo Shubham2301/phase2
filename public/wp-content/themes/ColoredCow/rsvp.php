527403f290ccef40c7468ebc519015c162988632
Shubham Joshi <shubhamjoshi2301@gmail.com>
2017-05-28 12:11:03 +0530

    #2 code correction after feedback

--- public/wp-content/themes/ColoredCow/rsvp.php
+++ public/wp-content/themes/ColoredCow/rsvp.php
@@ -5,12 +5,12 @@
 ?>
 <?php get_header(); ?>
 
-	<?php 
-	echo '<div>';
-		echo '<label for="email">enter your email</label>';
-		echo "<br>";
-		echo '<input type="email" class="email" id="guest_email" required>';
-	echo '</div>';	 
-	?>
+	
+<div>
+	<label for="email">enter your email</label>
+	<br>
+	<input type="email" class="email" id="guest_email" required>
+</div>	 
+	
 
 <?php get_footer(); ?>
