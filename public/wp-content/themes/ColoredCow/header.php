620c3efe688c7f1120faf0cbdfe0c743a1261b61
Shubham Joshi <shubhamjoshi2301@gmail.com>
2017-06-03 15:53:08 +0530

    #22 all soiree link added on navbar

--- public/wp-content/themes/ColoredCow/header.php
+++ public/wp-content/themes/ColoredCow/header.php
@@ -9,13 +9,17 @@
     <?php wp_head(); ?>
     <nav class = "navbar navbar-static-top navbar-default navbar-inverse" role = "navigation">
         <div class = "navbar-header">
-            <a class = "navbar-brand" href = "#">SOIREE</a>
+            <a class = "navbar-brand" href = "<?php echo get_home_url(); ?>">SOIREE</a>
         </div>
-        <!-- <div>
+        <div>
             <ul class = "nav navbar-nav navbar-right" style= "margin-right: 0">
-                <li class = ""><a href = "#">RSVP</a></li>
+                <?php 
+                    $page = get_page_by_title("All Soirees");
+                    $link_all_soiree_page = get_page_link($page->ID) 
+                ?>
+                <li class = ""><a href = "<?php echo $link_all_soiree_page; ?>">All-Soirees</a></li>
             </ul>
-        </div> -->
+        </div>
     </nav>
 </head>
 
